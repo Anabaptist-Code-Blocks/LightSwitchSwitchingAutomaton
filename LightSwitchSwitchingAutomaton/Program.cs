f543fbfe470ced946174aa6a927d40a91519430b
Melvin Ray Herr <melvinray@abcsystems.dev>
2021-12-31 14:40:26 -0500

    Make sure each person starts with the right switch

--- LightSwitchSwitchingAutomaton/Program.cs
+++ LightSwitchSwitchingAutomaton/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 var lights = new bool[100];
 for (int person = 1; person <= 100; person++)
 {
-    for (int sw = 0; sw < lights.Length; sw += person)
+    for (int sw = person -1; sw < lights.Length; sw += person)
         lights[sw] = !lights[sw];
 
     Console.WriteLine($"Person: {person:0##} Lights On: {lights.Count(l => l):0##} Lights Off: {lights.Count(l => !l):0##} Lights: {string.Join(" ", lights.Select(l => l ? "1" : "0"))}");
